927287ba3befd2f82e43133cdb11556583de89d7
Mazen Amria <t.mazenamria@gmail.com>
2025-07-23 17:22:18 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -8,4 +8,3 @@ python scripts/tune_ppo.py --env="pointmass" --device="cuda:4" --wandb --wandb-t
 python scripts/tune_ppo.py --env="pointmass" --device="cuda:5" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
 python scripts/tune_ppo.py --env="pointmass" --device="cuda:6" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
 python scripts/tune_ppo.py --env="pointmass" --device="cuda:7" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
-
