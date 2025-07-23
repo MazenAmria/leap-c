3f5e045eaa8a18a862d0509f3ce40fd7ce5638c0
Mazen Amria <t.mazenamria@gmail.com>
2025-07-23 17:21:48 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-python scripts/tune_ppo.py --env="pointmass" --device="cuda:0" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass"
-python scripts/tune_ppo.py --env="pointmass" --device="cuda:1" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass"
-python scripts/tune_ppo.py --env="pointmass" --device="cuda:2" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass"
-python scripts/tune_ppo.py --env="pointmass" --device="cuda:3" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass"
-python scripts/tune_ppo.py --env="pointmass" --device="cuda:4" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass"
-python scripts/tune_ppo.py --env="pointmass" --device="cuda:5" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass"
-python scripts/tune_ppo.py --env="pointmass" --device="cuda:6" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass"
-python scripts/tune_ppo.py --env="pointmass" --device="cuda:7" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass"
+python scripts/tune_ppo.py --env="pointmass" --device="cuda:0" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
+python scripts/tune_ppo.py --env="pointmass" --device="cuda:1" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
+python scripts/tune_ppo.py --env="pointmass" --device="cuda:2" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
+python scripts/tune_ppo.py --env="pointmass" --device="cuda:3" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
+python scripts/tune_ppo.py --env="pointmass" --device="cuda:4" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
+python scripts/tune_ppo.py --env="pointmass" --device="cuda:5" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
+python scripts/tune_ppo.py --env="pointmass" --device="cuda:6" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
+python scripts/tune_ppo.py --env="pointmass" --device="cuda:7" --wandb --wandb-team="amriam-university-of-freiburg" --output-path="tuning/ppo/pointmass" &
+
